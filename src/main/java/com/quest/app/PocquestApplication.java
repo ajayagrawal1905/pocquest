a473bebd0448cdb564546ec2e9d8b8b80407f5ca
Ajay Agrawal <ajayagrawal1905@gmail.com>
2021-10-14 16:23:43 +0530

    inital Commit

---
+++ src/main/java/com/quest/app/PocquestApplication.java
@@ -0,0 +1,13 @@
+package com.quest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quest/app/PocquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
